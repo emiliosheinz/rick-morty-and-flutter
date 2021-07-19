0c9bba980652da8838f2251c6d7dc309d7aca207
Emilio Heinzmann <emiliosheinz@gmail.com>
2021-07-19 14:31:40 -0300

    test: :white_check_mark: add character bloc tests

--- lib/modules/character/presentation/bloc/character_bloc.dart
+++ lib/modules/character/presentation/bloc/character_bloc.dart
@@ -3,17 +3,28 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:rick_morty_and_flutter/modules/character/domain/entities/character.dart';
+import 'package:rick_morty_and_flutter/modules/character/domain/use_cases/get_characters.dart';
 
 part 'character_event.dart';
 part 'character_state.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server failure';
+
 class CharacterBloc extends Bloc<CharacterEvent, CharacterState> {
-  CharacterBloc() : super(Empty());
+  final GetCharacters getCharacters;
+
+  CharacterBloc({required this.getCharacters}) : super(Empty());
 
   @override
   Stream<CharacterState> mapEventToState(
     CharacterEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is GetCharactersEvent) {
+      final getCharactersEither = await getCharacters();
+
+      yield* getCharactersEither.fold((failure) async* {
+        yield Error(message: SERVER_FAILURE_MESSAGE);
+      }, (r) => throw UnimplementedError());
+    }
   }
 }
---
+++ test/modules/home/presentation/bloc/character_bloc_test.dart
@@ -0,0 +1,53 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:rick_morty_and_flutter/core/error/failures.dart';
+import 'package:rick_morty_and_flutter/modules/character/domain/entities/character.dart';
+import 'package:rick_morty_and_flutter/modules/character/domain/use_cases/get_characters.dart';
+import 'package:rick_morty_and_flutter/modules/character/presentation/bloc/character_bloc.dart';
+
+import 'character_bloc_test.mocks.dart';
+
+@GenerateMocks([GetCharacters])
+main() {
+  late CharacterBloc bloc;
+  late MockGetCharacters getCharacters;
+
+  setUp(() {
+    getCharacters = MockGetCharacters();
+    bloc = CharacterBloc(getCharacters: getCharacters);
+  });
+
+  test('initialState should be Empty', () {
+    expect(bloc.state, Empty());
+  });
+
+  group('getCharacters', () {
+    final characters = [
+      Character(id: 1, name: "Nome Teste", image: "image.png"),
+      Character(id: 2, name: "Nome Teste 2", image: "image2.png")
+    ];
+
+    test('should call the getCharacters use case', () async {
+      when(getCharacters()).thenAnswer((_) async => Right(characters));
+
+      bloc.add(GetCharactersEvent());
+      await untilCalled(getCharacters());
+
+      verify(getCharacters());
+    });
+
+    test('should emit [Error] state when the server fails', () async {
+      when(getCharacters()).thenAnswer((_) async => Left(ServerFailure()));
+
+      final events = [Error(message: SERVER_FAILURE_MESSAGE)];
+      expectLater(
+        bloc.stream,
+        emitsInOrder(events),
+      );
+
+      bloc.add(GetCharactersEvent());
+    });
+  });
+}
