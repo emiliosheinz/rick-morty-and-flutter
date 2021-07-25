baebeecfced6f658c2258bf29359712af6bffe45
Emilio Heinzmann <emiliosheinz@gmail.com>
2021-07-25 12:39:19 -0300

    feat: add getScreenArguments to navigation service

--- lib/core/services/navigation.dart
+++ lib/core/services/navigation.dart
@@ -3,7 +3,17 @@ import 'package:flutter/material.dart';
 class NavigationService {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-  Future<dynamic> navigateTo(String routeName) {
-    return navigatorKey.currentState!.pushNamed(routeName);
+  Future<dynamic> navigateTo(
+    String routeName, {
+    Object? arguments,
+  }) {
+    return navigatorKey.currentState!.pushNamed(
+      routeName,
+      arguments: arguments,
+    );
+  }
+
+  T getScreenArguments<T>(BuildContext context) {
+    return ModalRoute.of(context)!.settings.arguments as T;
   }
 }
